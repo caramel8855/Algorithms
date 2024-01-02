9fe03e142e136f6cf95424f11f9bfe4f0a5e8114
Caramel <134868651+caramel8855@users.noreply.github.com>
2024-01-02 20:00:30 +0600

    Selection sorting in C

---
+++ Selection sort/selection_sorting.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+void printarray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << "\t";
+    }
+    std::cout << std::endl;
+}
+
+void selectionsort(int arr[], int n) {
+    int IndexMin, temp;
+    for (int i = 0; i < (n - 1); i++) {
+        IndexMin = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[IndexMin]) {
+                IndexMin = j;
+            }
+        }
+        // Swapping
+        temp = arr[i];
+        arr[i] = arr[IndexMin];
+        arr[IndexMin] = temp;
+    }
+}
+
+int main() {
+    int array[] = {2, 56, 48, 5, 34, 3};
+    int size = 6;
+    printarray(array, size);
+    selectionsort(array, size);
+    std::cout << "After selection sort: \n";
+    printarray(array, size);
+}
+
+// In this sort:
+// Total passes   : n-1
+// Time complexity : O(n^2)
+// Stable? : No
+// Adaptive? : No
