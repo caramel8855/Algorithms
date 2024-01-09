ca5520801518c09a8c2395cc33a8afe12056a70b
Caramel <134868651+caramel8855@users.noreply.github.com>
2024-01-09 12:58:50 +0600

    quick sorting in C plus plus

---
+++ Quick sort/quick_sortin.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+void printarray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << "\t";
+    }
+    cout << endl;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[low];
+    int i = low + 1;
+    int j = high;
+    int temp;
+
+    do {
+        while (arr[i] <= pivot) {
+            i++;
+        }
+        while (arr[j] > pivot) {
+            j--;
+        }
+        // swapping
+        if (i < j) {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    } while (i < j);
+    // swapping
+    temp = arr[j];
+    arr[j] = arr[low];
+    arr[low] = temp;
+    return j;
+}
+
+void quicksort(int arr[], int low, int high) {
+    int partitionIndex;
+    if (low < high) {
+        partitionIndex = partition(arr, low, high);
+        quicksort(arr, low, partitionIndex - 1);
+        quicksort(arr, partitionIndex + 1, high);
+    }
+}
+
+int main() {
+    int array[] = {7, 43, 5, 67, 21, 2};
+    int size = 6;
+    printarray(array, size);
+    quicksort(array, 0, size - 1);
+    cout << "After quick sort : \n";
+    printarray(array, size);
+    return 0;
+}
