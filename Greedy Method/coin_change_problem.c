cdf7ba078c0e847d4df248a00a9f5a0bb9a5c7a3
Caramel <134868651+caramel8855@users.noreply.github.com>
2024-03-20 21:58:47 +0600

    Update coin_change_problem.c

--- Greedy Method/coin_change_problem.c
+++ Greedy Method/coin_change_problem.c
@@ -10,7 +10,7 @@ void minimumcoins(int coin[], int n, int amount){
 }
 
 int main (){
-    int coin[] = {1, 2 ,5,10,20,50,100,200,500,100};
+    int coin[] = {1, 2 ,5,10,20,50,100,200,500,1000};
     int numofcoins = sizeof(coin)/sizeof(coin[0]);
 
     int amount;
